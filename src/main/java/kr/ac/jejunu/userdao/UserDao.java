0801dbb5ee54f2fd271cce717fceee3dfb67533a
ssoso27 <sohi8290@naver.com>
2019-04-19 14:23:20 +0900

    [11] Template Callback Pattern

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -11,22 +11,53 @@ public class UserDao {
     }
 
     public User get(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new GetStatementStrategy(id);
+        StatementStrategy statementStrategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+                preparedStatement.setLong(1, id);
+                return preparedStatement;
+            }
+        };
         return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
     public Long add(User user) throws SQLException {
-        StatementStrategy statementStrategy = new AddStatementStrategy(user);
+        StatementStrategy statementStrategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO userinfo(name, password) VALUES (?, ?);");
+                preparedStatement.setString(1, user.getName());
+                preparedStatement.setString(2, user.getPassword());
+                return preparedStatement;
+            }
+        };
         return jdbcContext.jdbcContextForAdd(statementStrategy);
     }
 
     public void update(User user) {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        StatementStrategy statementStrategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE userinfo SET name=?, password=? WHERE id=?; ");
+                preparedStatement.setString(1, user.getName());
+                preparedStatement.setString(2, user.getPassword());
+                preparedStatement.setLong(3, user.getId());
+                return preparedStatement;
+            }
+        };
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Long id) {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        StatementStrategy statementStrategy = new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM userinfo WHERE id=?;");
+                preparedStatement.setLong(1, id);
+                return preparedStatement;
+            }
+        };
         jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 }
